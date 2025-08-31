2c3da8bfbbfa4c7e05db6413a41bf8c2c13797e5
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-31 14:25:53 +0530

    Add files via upload

--- data/initialNotes.ts
+++ data/initialNotes.ts
@@ -1,3 +1,9 @@
 import type { Note } from '../types';
 
-export const initialNotes: Note[] = [];
+export const initialNotes: Note[] = [
+  {
+    "id": "2025-08-31T08:53:23.994Z",
+    "title": "AQI Index (BUDGET)",
+    "code": "ESP32 Dev Board  -\t₹479\n\nGas Sensor MQ-135  - ₹235\n\nLM358P Op-Amp DIP-8\t - ₹20\n\nDHT11 Module  -\t₹59\n\nMiscellaneous Components\t-\t₹100\n\nPower Supply  -\t₹100\n\nTotal Estimated Cost  ~  ₹1000"
+  }
+];
